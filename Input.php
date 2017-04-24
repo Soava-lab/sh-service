84b0af1925dd764154fe4ba17f2c3d3e3b2bc017
Soava-lab <32deva@phpbala.in>
2017-04-24 11:41:00 +0530

    Update Input.php

--- Input.php
+++ Input.php
@@ -1,13 +1,13 @@
 <?php
 if(!defined("SHA")) die("Access denied!");
 class Input{ 
-	public function post($var=NULL){
-		return ($var!=NULL)?$_POST[$var]:$_POST;
+	public function post($var=NULL){ 
+		return ($var!=NULL && isset($_POST[$var]))?$_POST[$var]:$_POST;
 	}
 	public function get($var=NULL){
-		return ($var!=NULL)?$_GET[$var]:$_GET;
+		return ($var!=NULL && isset($_GET[$var]))?$_GET[$var]:$_GET;
 	}
 	public function request($var=NULL){
-		return ($var!=NULL)?$_REQUEST[$var]:$_REQUEST;
+		return ($var!=NULL && isset($_REQUEST[$var]))?$_REQUEST[$var]:$_REQUEST;
 	}
 }
