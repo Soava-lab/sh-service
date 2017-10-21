a1d6ce6ed560d7aaed52939f1ba4fb4505dbb50a
Soava-lab <32deva@phpbala.in>
2017-10-21 15:50:02 +0530

    Response json and send options with headers
    Response json and send options with headers

--- Response.php
+++ Response.php
@@ -15,4 +15,24 @@ class Response{
 			die(self::setHeader("200",$content));
 		}
 	}
+	public static function send(){ $status = 200; $content_type = 'application/json';
+		$args = func_get_args();
+		if(count($args) >0){
+				if(count($args) == 1){
+					$body = $args[0];
+				}elseif(count($args) == 2){
+					$status = $args[0]; 
+					$body = $args[1];
+				}else{
+					$status = $args[0]; 
+					$body = $args[1];
+					$content_type = $args[2];
+				}
+				header("HTTP/1.1 ".$status."");
+				header("Content-Type: ".$content_type);
+				$send = json_encode(["status"=>$status,"body"=>$body]);
+				die($send);
+		}
+		
+	}
 }
