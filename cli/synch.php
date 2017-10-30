980888ed30c3add211f7dbf328e701dfa31d064d
Soava-lab <32deva@phpbala.in>
2017-10-30 17:55:17 +0530

    permission allow
    permission allow

--- cli/synch.php
+++ cli/synch.php
@@ -7,15 +7,18 @@ if($data->type!="" && $data->dest!="" && $data->content!=""){
 	if(file_exists($file)) die(clean_color("\033[0;31mSorry, ".ucfirst($type)." ".ucfirst($data->dest).".php already exist. \033[0m"));
 	$content= base64_decode($data->content);
 	if($type == 'api'){
+		if(is_dir("../extender/sh")) { $uold = umask(0); chmod("../extender/sh",0777,true); umask($uold); }else{
+			$uold = umask(0); mkdir("../extender/sh",0777,true); umask($uold);
+		}
 		$file = "../extender/sh/".ucfirst($data->dest).".php";
 		if(file_put_contents($file,$content)){
 			echo clean_color("\033[0;32m".ucfirst($type).' '.ucfirst($data->dest).'.php has been moved to server'."\033[0m");
 		}else{
 			echo clean_color("\033[0;31mSorry, ".ucfirst($type)." directory permission denied. \033[0m");
 		}
-	}else if($type == 'module'){
+	}else if($type == 'module'){ $uold = umask(0); chmod("../modules",0777,true); umask($uold);
 		
-	}else{
+	}else{ $uold = umask(0); chmod("../".$type,0777,true); umask($uold);
 		if(file_put_contents($file,$content)){
 			echo clean_color("\033[0;32m".ucfirst($type).' '.ucfirst($data->dest).'.php has been moved to server'."\033[0m");
 		}else{
