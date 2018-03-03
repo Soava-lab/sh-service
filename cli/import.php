5111120c48551775f46d61ef38585f07ba715f6b
Soava-lab <32deva@phpbala.in>
2018-03-03 11:37:19 +0530

    Update import.php

--- cli/import.php
+++ cli/import.php
@@ -48,7 +48,7 @@ class import{
 		$message = "<?php\nnamespace package;\nclass ".ucfirst($fileName)."{\n\n}\n?>";
 		if (file_exists($file)){
 		  	$msg = "\033[0;31m".ucfirst($fileName)." ".$c_dir." already exist.\033[0m \n";
-		} else {  $uold = umask(0); mkdir($c_dir,0777,true); umask($uold); 
+		} else {  $uold = umask(0); mkdir($c_dir,0777); umask($uold); 
 		  if(is_dir($c_dir) && is_writable($c_dir)){
 		  	# Download from server & extract
 			$url = $this->server_uri.$c_dir."/".ucfirst($fileName).".pkg";
@@ -73,7 +73,7 @@ class import{
 		$message = "<?php\nnamespace package;\nclass ".ucfirst($fileName)."{\n\n}\n?>";
 		if (file_exists($file)){
 		  	$msg = "\033[0;31m".ucfirst($fileName)." ".$c_dir." already exist.\033[0m \n";
-		} else {  $uold = umask(0); mkdir($c_dir,0777,true); umask($uold); 
+		} else {  $uold = umask(0); mkdir($c_dir,0777); umask($uold); 
 		  if(is_dir($c_dir) && is_writable($c_dir)){
 		  	# Download from server & extract
 			$url = $this->server_uri.$c_dir."/".ucfirst($fileName).".pkg";
@@ -98,7 +98,7 @@ class import{
 		if(is_dir($module)){
 		  	$msg = "\033[0;31m".$fileName." module already exist.\033[0m \n";
 		}else{ 
-		  if(!is_dir($c_dir)){ $uold = umask(0); mkdir($c_dir,0777,true); umask($uold); }
+		  if(!is_dir($c_dir)){ $uold = umask(0); mkdir($c_dir,0777); umask($uold); }
 		  if(is_dir($c_dir) && is_writable($c_dir)){
 		  	# Download from server & extract
 			$url = $this->server_uri.$c_dir."/".strtolower($fileName).".zip";
@@ -130,7 +130,7 @@ class import{
 		if(is_dir($module)){
 		  	$msg = "\033[0;31m".$fileName." module already exist.\033[0m \n";
 		}else{ 
-		  if(!is_dir($c_dir)){ $uold = umask(0); mkdir($c_dir,0777,true); umask($uold); }
+		  if(!is_dir($c_dir)){ $uold = umask(0); mkdir($c_dir,0777); umask($uold); }
 		  if(is_dir($c_dir) && is_writable($c_dir)){
 		  	# Download from server & extract
 		  	# https://github.com/Soava-lab/warehouse/raw/master/modules/register.zip
