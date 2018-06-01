3d20394394dcb5975b2e67ac7f27a1e18193e3d5
Soava-lab <ctmiseapp@gmail.com>
2018-06-01 12:29:34 +0530

    Add files via upload

---
+++ 400.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>400 Bad Request</title>
+</head>
+<body>
+<h2 align="center"><small>400 Bad Request</small></h2>
+</body>
+</html>
